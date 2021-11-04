2758b5dd9df635b457a31276035b244779e20c91
gersoncjlima <gersoncjlima@gmail.com>
2021-11-04 11:07:52 -0300

    Static charts

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -13,42 +13,42 @@ const DataTable = () => {
                 </thead>
                 <tbody>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>01/11/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>03/11/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>03/11/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>03/11/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>04/11/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
                     <tr>
-                        <td>22/04/2021</td>
+                        <td>04/11/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
