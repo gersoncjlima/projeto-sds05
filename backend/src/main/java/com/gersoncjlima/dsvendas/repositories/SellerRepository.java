ceb1324ceafe16a7e91cad1bcc262d7d2775c6c1
gersoncjlima <gersoncjlima@gmail.com>
2021-11-07 11:10:34 -0300

    Layers created

---
+++ backend/src/main/java/com/gersoncjlima/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.gersoncjlima.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gersoncjlima.dsvendas.entites.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+
+}
